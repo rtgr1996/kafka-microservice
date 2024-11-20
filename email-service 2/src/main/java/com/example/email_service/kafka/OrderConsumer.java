08f8e5e5e9b02d7f3e985687bef711dbb5a7e7f5
rohit <rohit.gaur@bluestone.com>
2024-11-20 23:54:01 +0530

    added email sending logic

--- email-service 2/src/main/java/com/example/email_service/kafka/OrderConsumer.java
+++ email-service 2/src/main/java/com/example/email_service/kafka/OrderConsumer.java
@@ -4,6 +4,8 @@ import com.example.base_domains.dto.OrderEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +13,21 @@ public class OrderConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(OrderConsumer.class);
 
+    private final JavaMailSender mailSender;
+
+    public OrderConsumer(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     @KafkaListener(topics = "${spring.kafka.topics}", groupId = "${spring.kafka.consumer.group-id}")
     public void consumer(OrderEvent orderEvent){
         log.info("event received in email service " + orderEvent.toString());
-        //save order event -- use wikimedia if required
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("rtgr1996@gmail.com");
+        message.setSubject("Order Confirmed");
+        message.setText("Order Confirmed " + orderEvent.toString());
+        message.setFrom("rohit.gaur0@gmail.com");
+        mailSender.send(message);
     }
 
 }
